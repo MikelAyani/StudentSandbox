ea38f211500d527e86f9934c7133bc6267bbbeff
Mikel Ayani <mikel.ayani@simumatik.com>
2020-02-18 21:48:47 +0100

    Example code to estart implementation

--- camera_sandbox.py
+++ camera_sandbox.py
@@ -6,12 +6,45 @@ The run method inside this script will be called each time the camera is require
 
 transform: refers to a vector including position and transformation (quaternion) data [x, y, z, qx, qy, qz, qw].
 
-settings:
-    - Width (int): camera width pixels 
-    - ...
+settings: A dictionary containing the camera settings
+    'frame': (float[7]) camera global frame [x, y, z, qx, qy, qz, qw],
+    'width': (int) camera width pixels, 
+    'height': (int) camera height pixels,
+    'vertical_fov':  (float) camera vertical fov in degrees,
+    'near':  (float) camera near plane,
+    'far':  (float) camera far plane,
+    'format':  (str) camera image format: 'L', 'RGB', 'D' or 'RGBD',
+    'output_path': (str) camera image output path (filename),
 
-data:
-    
+data: A dictionary containing objects to be rendered.
+    'object_name':
+        'frame': (float[7]) object global frame [x, y, z, qx, qy, qz, qw],
+        'shapes': a dictionary of shapes included in the object
+            'shape_name':
+                'origin': (optional) (float[7]) shape local frame relative to the object [x, y, z, qx, qy, qz, qw],
+                'type': (str) shape type: 'plane', 'box', 'cylinder', 'sphere', 'capsule', 'mesh'
+                'attributes': shape specific attributes
+                    # plane
+                    'normal': (float[3]) x, y, z normal vector of the plane
+
+                    # box
+                    'size': (float[3]) x, y, z sizes of the box
+
+                    # cylinder
+                    'radius': (float) radius of the cylinder
+                    'length': (float) length of the cylinder
+
+                    # capsule
+                    'radius': (float) radius of the capsule
+                    'length': (float) length of the capsule
+
+                    # sphere
+                    'radius': (float) radius of the sphere
+
+                    # mesh
+                    'model': (str) path to the mesh model (GLB file)
+                    'scale': (float[3]) x, y, z axis scale values of the mesh
+
 """
 
 # Imports
@@ -28,5 +61,14 @@ def run(
     data: dict
     ):
 
-    print(settings)
-    print(data)
+    # To load camera settings
+    width = settings.get('width', 800)
+    height = settings.get('height', 600)
+    print(f"camera size is {width} x {height}")
+
+    for object_name, object_data in data.items():
+        num_shapes = len(object_data.get('shapes'))
+        print(f"rendering {object_name} with {num_shapes} shapes...")
+    
+    filename = settings.get('output_path', 'None')
+    print(f"saving results to {filename}...")
